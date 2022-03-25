14bc01e79aaf7cb17540017248c99e4cf077e93e
Tanuja pandey <pandey.tanuja2015@gmail.com>
2022-03-25 18:19:35 +0530

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Insert = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblProfit = new System.Windows.Forms.Label();
+            this.cmbName = new System.Windows.Forms.ComboBox();
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.txtProfit = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.dataDisplay = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(209, 258);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(94, 29);
+            this.Insert.TabIndex = 0;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(76, 54);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(49, 20);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(76, 114);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(37, 20);
+            this.lblYear.TabIndex = 3;
+            this.lblYear.Text = "Year";
+            // 
+            // lblProfit
+            // 
+            this.lblProfit.AutoSize = true;
+            this.lblProfit.Location = new System.Drawing.Point(76, 171);
+            this.lblProfit.Name = "lblProfit";
+            this.lblProfit.Size = new System.Drawing.Size(49, 20);
+            this.lblProfit.TabIndex = 4;
+            this.lblProfit.Text = "Profit ";
+            // 
+            // cmbName
+            // 
+            this.cmbName.FormattingEnabled = true;
+            this.cmbName.Items.AddRange(new object[] {
+            "Pankaj",
+            "Rahul",
+            "Sandeep"});
+            this.cmbName.Location = new System.Drawing.Point(193, 54);
+            this.cmbName.Name = "cmbName";
+            this.cmbName.Size = new System.Drawing.Size(151, 28);
+            this.cmbName.TabIndex = 5;
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Items.AddRange(new object[] {
+            "2020",
+            "2021",
+            "2022"});
+            this.cmbYear.Location = new System.Drawing.Point(193, 98);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(151, 28);
+            this.cmbYear.TabIndex = 6;
+            // 
+            // txtProfit
+            // 
+            this.txtProfit.Location = new System.Drawing.Point(193, 168);
+            this.txtProfit.Name = "txtProfit";
+            this.txtProfit.Size = new System.Drawing.Size(151, 27);
+            this.txtProfit.TabIndex = 7;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(514, 258);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(94, 29);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show Profit";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // dataDisplay
+            // 
+            this.dataDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataDisplay.Location = new System.Drawing.Point(417, 43);
+            this.dataDisplay.Name = "dataDisplay";
+            this.dataDisplay.RowHeadersWidth = 51;
+            this.dataDisplay.RowTemplate.Height = 29;
+            this.dataDisplay.Size = new System.Drawing.Size(300, 188);
+            this.dataDisplay.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataDisplay);
+            this.Controls.Add(this.txtProfit);
+            this.Controls.Add(this.cmbYear);
+            this.Controls.Add(this.cmbName);
+            this.Controls.Add(this.lblProfit);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.Insert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblProfit;
+        private System.Windows.Forms.ComboBox cmbName;
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.TextBox txtProfit;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.DataGridView dataDisplay;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void insertRecordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Employee_profitSum;Integrated Security=True");
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+           
+                
+                string empname = cmbName.Text;
+                string empyear = cmbYear.Text;
+                int empprofit = int.Parse(txtProfit.Text);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("exec Insert_Emp '" + empname + "','" + empyear + "','" + empprofit + "'", con);
+                cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully inserted.....!");
+            con.Close();
+            }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            GetProfit();
+        }
+
+       
+        void GetProfit()
+        {
+            SqlCommand cmd = new SqlCommand("exec Sum_Emp_Profit ", con);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataDisplay.DataSource = dt;
+        }
+    }
+    }
